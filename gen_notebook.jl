04c5c729590fe01f63affe1255f2862a39a3d6d4
Hans Würfel <git@wuerfel.io>
2025-04-14 13:41:05 +0200

    allow to mark lines in notebook to not include in released notebook

--- gen_notebook.jl
+++ gen_notebook.jl
@@ -15,8 +15,17 @@ using Literate
 template = joinpath(BASEDIR, "notebook_template.jl")
 @assert isfile(template)
 
+# Define custom preprocessing function to filter #norelease lines
+function filter_norelease(content)
+    lines = split(content, '\n')
+    filtered_lines = filter(line -> !contains(line, "#norelease"), lines)
+    return join(filtered_lines, '\n')
+end
+
+# generate two versions: filtered and non filtered
 TMPDIR = mktempdir()
-Literate.notebook(template, TMPDIR; execute=false, name="workshop")
+Literate.notebook(template, TMPDIR; execute=false, name="workshop", preprocess=filter_norelease)
+Literate.notebook(template, TMPDIR; execute=false, name="workshop_full")
 
 using TOML
 project = TOML.parsefile(joinpath(BASEDIR, "Project.toml"))
@@ -24,8 +33,9 @@ project = TOML.parsefile(joinpath(BASEDIR, "Project.toml"))
 companion_name = project["name"]
 companion_uuid = project["uuid"]
 
+# change name of env
+project["name"] = "OpPoDynWorkshopEnv"
 # remove the blocsk we don't need
-delete!(project, "name")
 delete!(project, "uuid")
 delete!(project, "authors")
 delete!(project, "version")
@@ -38,8 +48,8 @@ end
 
 project["deps"][companion_name] = companion_uuid
 
+# for release, link to gitub. For local dev use local link
 project["sources"][companion_name] = if haskey(ENV, "GITHUB_ACTIONS")
-    # Available automatically in GitHub Actions
     Dict(
         "url" => "https://github.com/hexaeder/OpPoDyn_FNESA_2025",
         "rev" => ENV["GITHUB_SHA"]
@@ -48,7 +58,7 @@ else
     Dict("path" => "..",)
 end
 
-
+# Save Project.toml for the notebook
 open(joinpath(TMPDIR, "Project.toml"), "w") do io
     TOML.print(io, project)
 end
@@ -70,3 +80,14 @@ else
     @info "Create notebook/workshop.ipynb"
 end
 mv(joinpath(TMPDIR, "workshop.ipynb"), joinpath(NBDIR, "workshop.ipynb"))
+
+# Move full version for testing
+if isfile(joinpath(NBDIR, "workshop_full.ipynb"))
+    @info "Replace notebook/workshop_full.ipynb with new version"
+    rm(joinpath(NBDIR, "workshop_full.ipynb"))
+else
+    @info "Create notebook/workshop_full.ipynb"
+end
+mv(joinpath(TMPDIR, "workshop_full.ipynb"), joinpath(NBDIR, "workshop_full.ipynb"))
+
+@info "Generated notebook files in notebook/ directory"
